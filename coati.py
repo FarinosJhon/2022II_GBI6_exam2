b7c9489f98d98adb8d1574e058b2e9cd738cfe3e
FarinosJhon <jhon.farinos@est.ikiam.edu.ec>
2023-02-01 19:59:09 -0500

    Jhon Fariños ha editado la información de README.md

--- coati.py
+++ coati.py
@@ -1,1 +1,15 @@
-jeje
+def fasta_downloader(coati):
+    """
+         Carga id_coati.txt en id_coatiy, descarga en formato genbank la información correspondiente a los identificadores de accesión usando el ENTREZ de Biopythony guarda en coati y en coati.gb
+    """
+    from Bio import Entrez
+   
+    ids = []
+    with open("data/coati.txt", 'r+') as coati:
+    coati = coati.read()
+    line =  coati.split("\n")[0:]
+   
+      
+
+
+    
