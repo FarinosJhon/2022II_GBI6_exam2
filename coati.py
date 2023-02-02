ce529192652cd8b05321398a6744a3dbf8b425ac
FarinosJhon <jhon.farinos@est.ikiam.edu.ec>
2023-02-01 20:24:55 -0500

    Jhon subió lo que se intentó del código :c diosmio

--- coati.py
+++ coati.py
@@ -1,13 +1,16 @@
-def fasta_downloader(coati):
+from Bio import Entrez
+
+ids=""
+def fasta_downloader():
     """
          Carga id_coati.txt en id_coatiy, descarga en formato genbank la información correspondiente a los identificadores de accesión usando el ENTREZ de Biopythony guarda en coati y en coati.gb
     """
-    from Bio import Entrez
-   
-    ids = []
-    with open("data/coati.txt", 'r+') as coati:
-    coati = coati.read()
-    line =  coati.split("\n")[0:]
+    with open("data/coati.txt") as coati:
+    name = coati.readline()[0:-1]
+    for line in coati:
+        ids += line.replace("\n","")
+      
+
    
       
 
